e4141de19a424b08a4bf0b078f3a62648f483a94
Maciej <zero323@users.noreply.github.com>
2018-12-13 10:40:57 +0100

    Document table_utils.R (#20)

--- R/table_utilis.R
+++ R/table_utilis.R
@@ -1,10 +1,18 @@
+#' Conver MR analysis results to tangram object
+#'
+#' @param results list of the same shape as returned from \code{\link{run_mr}}
+#' @return a tangram object (see \code(\link[tangram]{tangram}} for details)
 #' @importFrom magrittr %>%
 #' @export
 tangram_results <- function(results, outcome) {
+  # An empty header row
   header_empty <- tangram::cell_header("")
+  # An empty subheader row
   subheader_empty <- tangram::cell_subheader("")
+  # An empty cell
   cell_empty <- tangram::cell("")
 
+  # Prepare header row
   tbl <- tangram::tangram(1, 1, id="results")
   tbl[[1]][[1]] <- tangram::cell_header("Trait")
   tbl[[1]][[2]] <- tangram::cell_header("MR methodology")
@@ -18,6 +26,7 @@ tangram_results <- function(results, outcome) {
   tbl[[1]][[10]] <- header_empty
   tbl[[1]][[11]] <- tangram::cell_header("Power")
 
+  # Prepare subheader row
   tbl[[2]] <- lapply(1:11, function(...) subheader_empty)
   tbl[[2]][[3]] <- tangram::cell_subheader("Number of SNPs")
   tbl[[2]][[4]] <- tangram::cell_subheader("R2")
@@ -26,17 +35,25 @@ tangram_results <- function(results, outcome) {
   tbl[[2]][[7]] <- tangram::cell_subheader("95% CI")
   tbl[[2]][[8]] <- tangram::cell_subheader("p")
 
+  # Add empty row - we'll use it later to fill the data
   tbl[[3]] <- lapply(1:11, function(x) cell_empty)
 
+  # Prepare subtable representing results for a single trait
+  # See return type of compute_result_for_exposure
+  # for expected input
   make_trait_table <- function(result) {
     trait <- result$meta$exposure
 
+    # Combine results into a tidy table
+    # See combine_experiment_data docs for detaisl
     combined_data <- combine_experiment_data(
       trait, result$mr, result$heterogeneity_metrics, result$meta
     )
 
+    # Determine how many rows we need to represent all results
     nrow <- max(nrow(combined_data), length(result$heterogeneity_metrics))
 
+    # Create an empty witreframe
     trait_table <- lapply(1:nrow, function(...) lapply(1:11, function(...) cell_empty))
 
     # Set trait column
@@ -52,6 +69,7 @@ tangram_results <- function(results, outcome) {
       trait_table[[i]][[8]] <- tangram::cell(round(combined_data[i, "pval"], 4))
     }
 
+    # Set heterogenity metrics
     for(i in 1:length(result$heterogeneity_metrics)) {
       metric <- result$heterogeneity_metrics[i]
       trait_table[[i]][[9]] <- tangram::cell(names(metric))
@@ -69,7 +87,10 @@ tangram_results <- function(results, outcome) {
     trait_table
   }
 
+  # Compute partial results for each
   lapply(results, make_trait_table) %>%
+    # Merge the partial results into a single list
     do.call(c, .) %>%
+    # Fill the tangram
     magrittr::inset(tbl, 3:(2 + length(.)), .)
 }
